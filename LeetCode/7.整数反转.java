b6e379887f59cba412cf2154811ac8677aa2a21a
czyphantom <czyphantom@gmail.com>
2021-06-06 20:59:03 +0800

    add solution

---
+++ LeetCode/7.整数反转.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=java
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result < Integer.MIN_VALUE / 10 || result > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            result = result * 10 + digit;
+        }
+        return result;
+
+    }
+}
+// @lc code=end
+
