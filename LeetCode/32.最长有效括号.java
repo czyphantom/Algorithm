26b96f226467a3daf110114bf1ca18b5ddbea91c
zuoyao.czy <zuoyao.czy@alibaba-inc.com>
2021-07-08 21:23:09 +0800

    update solution

--- LeetCode/32.最长有效括号.java
+++ LeetCode/32.最长有效括号.java
@@ -1,9 +1,13 @@
-/**
- * 题目：给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+/*
+ * @lc app=leetcode.cn id=32 lang=java
+ *
+ * [32] 最长有效括号
+ * 题目：给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
  * 难度：Hard
- * 思路：用栈或者dp
+ * 思路：栈
  */
 
+// @lc code=start
 class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<Integer>();
@@ -30,9 +34,7 @@ class Solution {
         }
         return max;
     }
-}
 
-class Solution2 {
     public int longestValidParentheses(String s) {
         int[] dp = new int[s.length()];
         int result = 0;
@@ -49,4 +51,36 @@ class Solution2 {
         }
         return result;
     }
+
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
 }
+// @lc code=end
+
