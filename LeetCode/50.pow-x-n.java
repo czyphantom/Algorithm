2bb896a64ce92f78a6b0a77f908f1c8100616b08
zuoyao.czy <zuoyao.czy@alibaba-inc.com>
2021-07-22 17:23:36 +0800

    update

--- LeetCode/50.pow-x-n.java
+++ LeetCode/50.pow-x-n.java
@@ -1,23 +1,32 @@
-/**
- * 题目：实现pow(x, n)，即计算x的n次幂函数。
+/*
+ * @lc app=leetcode.cn id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ * 题目：实现 pow(x, n) ，即计算x的n次幂函数（即，x^n）。
  * 难度：Medium
- * 思路：快速幂，注意溢出的问题
+ * 思路：二分
  */
 
+// @lc code=start
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0) 
+        if(n == 0) {
             return 1;
-        if(n == 1) 
+        }
+        if(n == 1) {
             return x;
+        }
         int t = n/2;
         if(n < 0) {
             t = -t;
             x = 1 / x;
         }
         double res = myPow(x, t);
-        if(n % 2 == 0) 
+        if(n % 2 == 0) {
             return res * res;
+        }
         return res * res * x;
     }
 }
+// @lc code=end
+
