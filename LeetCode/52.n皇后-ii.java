c1aaa37bfef8c0668800173a6166047907b14a53
zuoyao.czy <zuoyao.czy@alibaba-inc.com>
2021-08-09 15:59:14 +0800

    update solution

--- LeetCode/52.n皇后-ii.java
+++ LeetCode/52.n皇后-ii.java
@@ -1,9 +1,14 @@
-/**
- * 题目：求n皇后问题解法的个数
+/*
+ * @lc app=leetcode.cn id=52 lang=java
+ *
+ * [52] N皇后 II
+ * 题目：n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
  * 难度：Hard
  * 思路：回溯
  */
 
+// @lc code=start
 class Solution {
     int count = 0;
     public int totalNQueens(int n) {
@@ -36,3 +41,5 @@ class Solution {
         }
     }
 }
+// @lc code=end
+
