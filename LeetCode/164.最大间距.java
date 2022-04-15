f121803dd9f041afbea9bd8777ade4bb36feccc9
zuoyao.czy <zuoyao.czy@alibaba-inc.com>
2022-04-15 16:34:32 +0800

    add solution

---
+++ LeetCode/164.最大间距.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=164 lang=java
+ *
+ * [164] 最大间距
+ * 题目：给定一个无序的数组nums，返回数组在排序之后，相邻元素之间最大的差值 。如果数组元素个数小于2，则返回0 。
+ * 难度：Hard
+ * 思路：基数排序或者桶
+ */
+
+// @lc code=start
+class Solution {
+    //桶
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        //桶的大小为(最大值-最小值)/n-1,相邻元素的差值不会超过桶的大小
+        int minVal = Arrays.stream(nums).min().getAsInt();
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int d = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketSize = (maxVal - minVal) / d + 1;
+
+        int[][] bucket = new int[bucketSize][2];
+        for (int i = 0; i < bucketSize; ++i) {
+            //存储 (桶内最小值，桶内最大值) 对， (-1, -1) 表示该桶是空的
+            Arrays.fill(bucket[i], -1); 
+        }
+        for (int i = 0; i < n; i++) {
+            int idx = (nums[i] - minVal) / d;
+            if (bucket[idx][0] == -1) {
+                bucket[idx][0] = bucket[idx][1] = nums[i];
+            } else {
+                bucket[idx][0] = Math.min(bucket[idx][0], nums[i]);
+                bucket[idx][1] = Math.max(bucket[idx][1], nums[i]);
+            }
+        }
+
+        int ret = 0;
+        int prev = -1;
+        for (int i = 0; i < bucketSize; i++) {
+            if (bucket[i][0] == -1) {
+                continue;
+            }
+            if (prev != -1) {
+                ret = Math.max(ret, bucket[i][0] - bucket[prev][1]);
+            }
+            prev = i;
+        }
+        return ret;
+    }
+
+    //基数排序
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        long exp = 1;
+        int[] buf = new int[n];
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+
+        while (maxVal >= exp) {
+            int[] cnt = new int[10];
+            for (int i = 0; i < n; i++) {
+                int digit = (nums[i] / (int) exp) % 10;
+                cnt[digit]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / (int) exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = Math.max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }\
+}
+// @lc code=end
+
