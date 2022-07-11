8cd1849fdff1f88d5822ba7a01ec1b449cedaaa5
czyphantom <czyphantom@gmail.com>
2022-07-11 20:52:18 +0800

    update

--- LeetCode/102.二叉树的层序遍历.java
+++ LeetCode/102.二叉树的层序遍历.java
@@ -2,7 +2,7 @@
  * @lc app=leetcode.cn id=102 lang=java
  *
  * [102] 二叉树的层序遍历
- * 题目：给你一个二叉树，请你返回其按 层序遍历得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 题目：给你一个二叉树，请你返回其按层序遍历得到的节点值。（即逐层地，从左到右访问所有节点）。
  * 难度：Medium
  * 思路：队列
  */
@@ -47,31 +47,6 @@ class Solution {
             }
             queue = newQueue;
             result.add(cur);
-        }
-        return result;
-    }
-
-    public List <List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Queue<TreeNode> queue =new LinkedList<>();
-        if(root == null){
-            return result;
-        }
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
-            int size = queue.size();
-            for(int i = 0;i < size;i++){
-                TreeNode node = queue.remove();
-                temp.add(node.val);
-                if(node.left != null){
-                    queue.offer(node.left);
-                }
-                if(node.right != null){
-                    queue.offer(node.right);
-                }
-            }
-            result.add(temp);
         }
         return result;
     }
