d3448cc0b11aca5514a00432caddfa23c978e6c4
czyphantom <czyphantom@gmail.com>
2022-07-20 22:31:56 +0800

    update solution

--- LeetCode/Math/136.只出现一次的数字.java
+++ LeetCode/Math/136.只出现一次的数字.java
@@ -10,9 +10,10 @@
 // @lc code=start
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-        for(int i=0;i<nums.length;i++)
+        int res = nums[0];
+        for (int i = 1;i < nums.length;i++) {
             res ^= nums[i];
+        }
         return res;
     }
 }
