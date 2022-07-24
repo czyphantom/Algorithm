90e8e059dd14c896821cd0eaf7c071efdca72050
czyphantom <czyphantom@gmail.com>
2022-07-24 22:08:42 +0800

    update solution

--- LeetCode/LinkedList/141.环形链表.java
+++ LeetCode/LinkedList/141.环形链表.java
@@ -21,8 +21,10 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
         while (fast != null) {
             slow = slow.next;
             fast = fast.next;
