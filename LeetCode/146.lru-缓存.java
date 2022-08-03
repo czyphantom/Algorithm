3328ed4404e3f0004cdc51cb2b6f8848d8437610
czyphantom <czyphantom@gmail.com>
2022-08-03 23:43:18 +0800

    update solution

--- LeetCode/146.lru-缓存.java
+++ LeetCode/146.lru-缓存.java
@@ -10,29 +10,6 @@
 // @lc code=start
 class LRUCache {
 
-    //直接用linkedhashmap
-    private LinkedHashMap<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
-
-    private int capacity = 0;
-
-    public LRUCache(int capacity) {
-        linkedHashMap = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > capacity;
-            }
-        };
-        this.capacity = capacity;
-    }
-    
-    public int get(int key) {
-        return linkedHashMap.getOrDefault(key, -1);
-    }
-    
-    public void put(int key, int value) {
-        linkedHashMap.put(key, value);
-    }
-
-    //标准写法,map+双向链表
     Map<Integer, Node> map;
     int capicity, count;
     Node head, tail;
@@ -48,50 +25,49 @@ class LRUCache {
         tail.next = null;
         count = 0;
     }
-
-    public void deleteNode(Node node) {
-        node.pre.next = node.next;
-        node.next.pre = node.pre;
-    }
-    
-    public void addToHead(Node node) {
-        node.next = head.next;
-        node.next.pre = node;
-        node.pre = head;
-        head.next = node;
-    }
     
     public int get(int key) {
         if (map.get(key) != null) {
             Node node = map.get(key);
             int result = node.value;
             deleteNode(node);
-            addToHead(node);
+            putAtFirst(node);
             return result;
         }
         return -1;
     }
     
-    public void set(int key, int value) {
+    public void put(int key, int value) {
         if (map.get(key) != null) {
             Node node = map.get(key);
             node.value = value;
             deleteNode(node);
-            addToHead(node);
+            putAtFirst(node);
         } else {
             Node node = new Node(key, value);
             map.put(key, node);
             if (count < capicity) {
                 count++;
-                addToHead(node);
+                putAtFirst(node);
             } else {
                 map.remove(tail.pre.key);
                 deleteNode(tail.pre);
-                addToHead(node);
+                putAtFirst(node);
             }
         }
     }
 
+    private void deleteNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    private void putAtFirst(Node node) {
+        node.next = head.next;
+        node.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
 }
 
 class Node {
--- LeetCode/147.对链表进行插入排序.java
+++ LeetCode/147.对链表进行插入排序.java
@@ -4,7 +4,7 @@
  * [147] 对链表进行插入排序
  * 题目：给定单个链表的头head ，使用插入排序对链表进行排序，并返回排序后链表的头。
  * 难度：Medium
- * 思路：用一个冗余节点保存会方遍一点
+ * 思路：记录前面有序链表的最后一个节点
  */
 
 // @lc code=start
@@ -19,6 +19,32 @@
  * }
  */
 class Solution {
+
+    //个人解法
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = head,pre = dummy;
+        while (p != null) {
+            ListNode q = dummy;
+            while (q.next != null && q.next != p && q.next.val <= p.val) {
+                q = q.next;
+            }
+            if (q.next == p) {
+                pre = pre.next;
+                p = p.next;
+                continue;
+            }
+            ListNode r = q.next,s = p.next;
+            q.next = p;
+            p.next = r;
+            pre.next = s;
+            p = s;
+        }
+        return dummy.next;
+    }
+
+    //标准解法
     public ListNode insertionSortList(ListNode head) {
         ListNode dummy = new ListNode(-1);
         ListNode pre = dummy;
@@ -36,5 +62,6 @@ class Solution {
         return dummy.next;
     }
 }
+
 // @lc code=end
 
