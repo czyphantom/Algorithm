8260d92f0b091b17b08364a3c243a0ebce70fe21
czyphantom <czyphantom@gmail.com>
2023-02-16 22:59:01 +0800

    update solution

--- LeetCode/BinarySearch/74.搜索二维矩阵.java
+++ LeetCode/BinarySearch/74.搜索二维矩阵.java
@@ -12,24 +12,18 @@
 // @lc code=start
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-        int row = matrix.length;
-        int column = matrix[0].length;
-        int total = row * column;
-        int low = 0;
-        int high = total-1;
-        while (low <= high) {
-            int mid = (low+high)/2;
-            int r = mid/column;
-            int c = mid%column;
-            if(target == matrix[r][c]) {
+        int m = matrix.length,n = matrix[0].length;
+        int begin = 0, end = m*n-1;
+        while (begin <= end) {
+            int mid = (end-begin)/2+begin;
+            int i = mid/n;
+            int j = mid - ((int)(mid/n)) * n;
+            if (matrix[i][j] < target) {
+                begin = mid+1;
+            } else if (matrix[i][j] == target) {
                 return true;
-            } else if (target > matrix[r][c]) {
-                low = mid+1;
             } else {
-                high = mid-1;
+                end = mid-1;
             }
         }
         return false;
--- LeetCode/Other/75.颜色分类.java
+++ LeetCode/Other/75.颜色分类.java
@@ -5,40 +5,11 @@
  * 题目：给定一个包含红色、白色和蓝色，一共n个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
  * 此题中，我们使用整数0、 1和2分别表示红色、白色和蓝色。
  * 难度：Medium
- * 思路：计数或者三向切分
+ * 思路：快排中的三向切分
  */
 
 // @lc code=start
 class Solution {
-    public void sortColors(int[] nums) {
-        int red = 0,white = 0,blue = 0;
-        for (int i = 0;i < nums.length;i++) {
-            if (nums[i] == 0) {
-                red++;
-            }
-            if (nums[i] == 1) {
-                white++;
-            }
-            if (nums[i] == 2) {
-                blue++;
-            }
-        }
-        for (int i = 0;i < nums.length;i++) {
-            if (red-- > 0) {
-                nums[i] = 0;
-                continue;
-            }
-            if (white-- > 0) {
-                nums[i] = 1;
-                continue;
-            }
-            if (blue-- > 0) {
-                nums[i] = 2;
-                continue;
-            }
-        }
-    }
-
     public void sortColors(int[] nums) {
         int begin = 0;
         int end = nums.length-1;
