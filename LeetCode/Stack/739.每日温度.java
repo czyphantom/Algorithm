af2bd3ce21c0f10c3a7d3c17cd571a8e6d3a9a80
czyphantom <czyphantom@gmail.com>
2023-05-15 22:45:32 +0800

    update solution

---
+++ LeetCode/Stack/739.每日温度.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=739 lang=java
+ *
+ * [739] 每日温度
+ * 题目：给定一个整数数组temperatures ，表示每天的温度，返回一个数组answer，其中answer[i]是指在第i天之后，才会有更高的温度。如果气温在这之后都不会升高，请在该位置用0来代替。
+ * 难度：Medium
+ * 思路：单调栈
+ */
+
+// @lc code=start
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 1;i < temperatures.length;i++) {
+            if (temperatures[stack.peek()] >= temperatures[i]) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                    int index = stack.pop();
+                    result[index] = i-index;
+                }
+                stack.push(i);
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
