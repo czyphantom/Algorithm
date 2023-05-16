097d68292889a01461160ad696ab1884cb143137
czyphantom <czyphantom@gmail.com>
2023-05-16 23:13:08 +0800

    update

--- LeetCode/String/647.回文子串.java
+++ LeetCode/String/647.回文子串.java
@@ -13,16 +13,16 @@
 // @lc code=start
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length(), ans = 0;
-        for (int i = 0; i < 2 * n - 1; ++i) {
-            int l = i / 2, r = i / 2 + i % 2;
-            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-                --l;
-                ++r;
-                ++ans;
+        int len = s.length(), result = 0;
+        for (int i = 0; i < 2 * len - 1; i++) {
+            int left = i / 2, right = i / 2 + i % 2;
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+                result++;
             }
         }
-        return ans;
+        return result;
     }
 }
 // @lc code=end
