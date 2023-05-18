d371afbddff95ce7dbe48d236123e7db99554997
czyphantom <czyphantom@gmail.com>
2023-05-18 23:01:30 +0800

    update solution

--- LeetCode/Greedy/581.最短无序连续子数组.java
+++ LeetCode/Greedy/581.最短无序连续子数组.java
@@ -4,13 +4,14 @@
  * [581] 最短无序连续子数组
  * 题目：给你一个整数数组nums，你需要找出一个连续子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
  * 难度：Medium
- * 思路：
+ * 思路：将数组分为三段，最左段和最右段都有序，对中间数组排序后整个数组有序，则最左段的数组的最大值<=后面数组的任意值，最右段数组的最小值>=前面数组的任意值
  */
 
 // @lc code=start
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
+        //maxn记录0-i-1的最大值，minn记录i+1-n的最小值
         int maxn = Integer.MIN_VALUE, right = -1;
         int minn = Integer.MAX_VALUE, left = -1;
         for (int i = 0; i < n; i++) {
--- LeetCode/HashTable/560.和为-k-的子数组.java
+++ LeetCode/HashTable/560.和为-k-的子数组.java
@@ -11,14 +11,14 @@
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0, pre = 0;
-        HashMap < Integer, Integer > mp = new HashMap < > ();
-        mp.put(0, 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             pre += nums[i];
-            if (mp.containsKey(pre - k)) {
-                count += mp.get(pre - k);
+            if (map.containsKey(pre - k)) {
+                count += map.get(pre - k);
             }
-            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
         }
         return count;
     }
