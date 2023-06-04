428ed40b7070a89467fa799fd09081001120661a
czyphantom <czyphantom@gmail.com>
2023-06-04 17:07:02 +0800

    update solution

--- LeetCode/DP/279.完全平方数.java
+++ LeetCode/DP/279.完全平方数.java
@@ -11,6 +11,32 @@
 class Solution {
 
     public int numSquares(int n) {
+        int[] squares = getSquares();
+        int[][] dp = new int[101][n+1];
+        for (int i = 0;i <= n;i++) {
+            dp[0][i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1;i <= 100;i++) {
+            for (int j = 1;j <= n;j++) {
+                if (j < squares[i]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-squares[i]]+1);
+                }
+            }
+        }
+        return dp[100][n];
+    }
+
+    private int[] getSquares() {
+        int[] squares = new int[101];
+        for (int i = 0;i <= 100;i++) {
+            squares[i] = i*i;
+        }
+        return squares;
+    }
+
+    public int numSquares(int n) {
         int[] f = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             int minn = Integer.MAX_VALUE;
