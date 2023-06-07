bbe13c8f4c76103da3923e3e76a5885fbcce43d3
czyphantom <czyphantom@gmail.com>
2023-06-07 23:03:34 +0800

    update solution

--- LeetCode/TopologySort/207.课程表.java
+++ LeetCode/TopologySort/207.课程表.java
@@ -10,6 +10,41 @@
 
 // @lc code=start
 class Solution {
+    
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> degreeMap = new HashMap<>();
+        Map<Integer, Set<Integer>> graphMap = new HashMap<>();
+        for (int i = 0;i < numCourses;i++) {
+            degreeMap.put(i, 0);
+            graphMap.put(i, new HashSet<>());
+        }
+        for (int i = 0;i < prerequisites.length;i++) {
+            degreeMap.put(prerequisites[i][0], degreeMap.get(prerequisites[i][0]) + 1);
+            graphMap.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        while (!degreeMap.isEmpty()) {
+            boolean hasZero = false;
+            List<Integer> removeKey = new ArrayList<>();
+            for (Map.Entry<Integer,Integer> entry : degreeMap.entrySet()) {
+                if (entry.getValue() == 0) {
+                    removeKey.add(entry.getKey());
+                    for (Integer node : graphMap.get(entry.getKey())) {
+                        degreeMap.put(node, degreeMap.get(node)-1);
+                    }
+                    graphMap.remove(entry.getKey());
+                    hasZero = true;
+                }
+            }
+            if (removeKey.size() != 0) {
+                removeKey.forEach(e -> degreeMap.remove(e));
+            }
+            if (!hasZero) {
+                return false;
+            } 
+        }
+        return true;
+    }
+
     List<List<Integer>> edges;
     int[] visited;
     boolean valid = true;
