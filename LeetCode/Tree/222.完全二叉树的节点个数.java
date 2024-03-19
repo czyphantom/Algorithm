c78473c0bdc428580474209d2f1e30acb92a8ef1
czyphantom <czyphantom@gmail.com>
2024-03-19 22:30:50 +0800

    add solution

---
+++ LeetCode/Tree/222.完全二叉树的节点个数.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ * 题目：给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+ * 难度：Easy
+ * 思路：利用完全二叉树的性质，节点个数在一个范围之内，在这个范围之内使用二分
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+}
+// @lc code=end
+
