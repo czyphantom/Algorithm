951ac13f8f9eb0a63e3c100eaa1d5d0cd95c8e4c
czyphantom <czyphantom@gmail.com>
2024-03-20 21:48:28 +0800

    add solution

---
+++ LeetCode/Stack/225.用队列实现栈.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=225 lang=java
+ *
+ * [225] 用队列实现栈
+ * 题目：请你仅使用两个队列实现一个后入先出（LIFO）的栈
+ * 难度：Easy
+ * 思路：
+ */
+
+// @lc code=start
+class MyStack {
+
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+    public int pop() {
+        return queue1.poll();
+    }
+    
+    public int top() {
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+// @lc code=end
+
