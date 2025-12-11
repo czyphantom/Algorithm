4e8db84cbfd8bc072e3c408621c73bd13282ad8a
czyphantom <czyphantom@gmail.com>
2025-12-11 22:32:55 +0800

    add solution

---
+++ LeetCode/Hot100/二叉树的右视图.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=java
+ *
+ * [199] 二叉树的右视图
+ * 题目：给定一个二叉树的根节点root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * 难度：Medium
+ * 思路：层序遍历每一层最右边一个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0;i < size;i++) {
+                TreeNode cur = queue.poll();
+                if (i == size-1) {
+                    result.add(cur.val);
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) {
+            return list;
+        }
+        dfs(root, 1);
+        return list;
+    }
+    void dfs(TreeNode root , int height){
+        if(root == null) {
+            return;
+        }
+        if(height > list.size()) {
+            list.add(root.val);
+        }
+        dfs(root.right, height + 1);
+        dfs(root.left, height + 1);
+    }
+}
+// @lc code=end
+
